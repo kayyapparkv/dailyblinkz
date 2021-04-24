a36687b9f17832f7c14801b98965f5c64a5f623a
Kumar Ayyappa <kumar.ayyappa@cubehighways.com>
2021-04-24 10:54:37 +0530

    moved from css to scss manually because some issues while convertiong from css to scss

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dailyBlinkzBootstrap'),
+      dir: require('path').join(__dirname, './coverage/dailyBlinkzBootstrapSCSS'),
       subdir: '.',
       reporters: [
         { type: 'html' },
