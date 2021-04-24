19d70510fd6ac0bbb3ecb7c128eabd0b80d5a397
Kumar Ayyappa <kumar.ayyappa@cubehighways.com>
2021-04-24 10:30:03 +0530

    removed css and added the scss stylings in the project

--- src/app/views/home-page/subscribe-component/subscribe-component.component.ts
+++ src/app/views/home-page/subscribe-component/subscribe-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-subscribe-component',
   templateUrl: './subscribe-component.component.html',
-  styleUrls: ['./subscribe-component.component.css']
+  styleUrls: ['./subscribe-component.component.scss']
 })
 export class SubscribeComponentComponent implements OnInit {
 
--- src/app/views/home-page/unsubscribe-component/unsubscribe-component.component.ts
+++ src/app/views/home-page/unsubscribe-component/unsubscribe-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-unsubscribe-component',
   templateUrl: './unsubscribe-component.component.html',
-  styleUrls: ['./unsubscribe-component.component.css']
+  styleUrls: ['./unsubscribe-component.component.scss']
 })
 export class UnsubscribeComponentComponent implements OnInit {
 
